4a126c97623f34eec9253f6123eb256bee8bc90d
wern <ancheta.wern@gmail.com>
2015-01-05 12:01:48 +0800

    solve issue #1

--- app/database/migrations/2014_10_19_225906_create_users_table.php
+++ app/database/migrations/2014_10_19_225906_create_users_table.php
@@ -5,28 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		$table->increments('id');
-        $table->string('username', 200);
-        $table->string('email', 160);
-        $table->string('password', 300);
-        $table->string('remember_token', 100);
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function($table){
+            $table->increments('id');
+            $table->string('username', 200);
+            $table->string('email', 160);
+            $table->string('password', 300);
+            $table->string('remember_token', 100);
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 
 }
