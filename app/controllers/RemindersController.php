f9626d458a2f2d504a27e05f1e71f934730c6e0f
wern <ancheta.wern@gmail.com>
2015-01-05 12:05:28 +0800

    pass in the message for the alert when submitting form for reminding password

--- app/controllers/RemindersController.php
+++ app/controllers/RemindersController.php
@@ -4,75 +4,77 @@ class RemindersController extends BaseController {
 
     protected $layout = 'layouts.default';
 
-	/**
-	 * Display the password reminder view.
-	 *
-	 * @return Response
-	 */
-	public function getRemind()
-	{
+    /**
+     * Display the password reminder view.
+     *
+     * @return Response
+     */
+    public function getRemind()
+    {
         $this->layout->title = 'Forgot Password';
-		$this->layout->content = View::make('password.remind');
-	}
+        $this->layout->content = View::make('password.remind');
+    }
 
-	/**
-	 * Handle a POST request to remind a user of their password.
-	 *
-	 * @return Response
-	 */
-	public function postRemind()
-	{
-		switch ($response = Password::remind(Input::only('email')))
-		{
-			case Password::INVALID_USER:
-				return Redirect::back()->with('error', Lang::get($response));
+    /**
+     * Handle a POST request to remind a user of their password.
+     *
+     * @return Response
+     */
+    public function postRemind()
+    {
 
-			case Password::REMINDER_SENT:
-				return Redirect::back()->with('status', Lang::get($response));
-		}
-	}
+        switch ($response = Password::remind(Input::only('email')))
+        {
+            case Password::INVALID_USER:
+                return Redirect::back()->with('message', array('type' => 'danger', 'text' => 'Something went wrong. Please try again.'));
 
-	/**
-	 * Display the password reset view for the given token.
-	 *
-	 * @param  string  $token
-	 * @return Response
-	 */
-	public function getReset($token = null)
-	{
-		if (is_null($token)) App::abort(404);
+            case Password::REMINDER_SENT:
+                return Redirect::back()->with('status', Lang::get($response))
+                    ->with('message', array('type' => 'success', 'text' => 'Password reminder sent! Check your inbox or spam folder to reset your password'));
+        }
+    }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * @param  string  $token
+     * @return Response
+     */
+    public function getReset($token = null)
+    {
+        if (is_null($token)) App::abort(404);
         $this->layout->title = 'Reset Password';
-		$this->layout->content = View::make('password.reset')->with('token', $token);
-	}
+        $this->layout->content = View::make('password.reset')->with('token', $token);
+    }
 
-	/**
-	 * Handle a POST request to reset a user's password.
-	 *
-	 * @return Response
-	 */
-	public function postReset()
-	{
-		$credentials = Input::only(
-			'email', 'password', 'password_confirmation', 'token'
-		);
+    /**
+     * Handle a POST request to reset a user's password.
+     *
+     * @return Response
+     */
+    public function postReset()
+    {
+        $credentials = Input::only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
 
-		$response = Password::reset($credentials, function($user, $password)
-		{
-			$user->password = Hash::make($password);
+        $response = Password::reset($credentials, function($user, $password)
+        {
+            $user->password = Hash::make($password);
 
-			$user->save();
-		});
+            $user->save();
+        });
 
-		switch ($response)
-		{
-			case Password::INVALID_PASSWORD:
-			case Password::INVALID_TOKEN:
-			case Password::INVALID_USER:
-				return Redirect::back()->with('error', Lang::get($response));
+        switch ($response)
+        {
+            case Password::INVALID_PASSWORD:
+            case Password::INVALID_TOKEN:
+            case Password::INVALID_USER:
+                return Redirect::back()->with('error', Lang::get($response));
 
-			case Password::PASSWORD_RESET:
-				return Redirect::to('/login')->with('message', array('type' => 'success', 'text' => 'Your password was updated! You can now login with your new password.'));
-		}
-	}
+            case Password::PASSWORD_RESET:
+                return Redirect::to('/login')->with('message', array('type' => 'success', 'text' => 'Your password was updated! You can now login with your new password.'));
+        }
+    }
 
 }
